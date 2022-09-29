b42e7bf7bf830c0dea01cb22235a657739dedf30
merlink <merlincita@gmail.com>
2022-09-29 11:41:58 +0200

    Feat: NavBar

--- Front/src/components/navbar.js
+++ Front/src/components/navbar.js
@@ -1,11 +1,55 @@
-const NavBar = ({ className }) => {
+import logo from "../assets/localib.png";
+
+const NavBar = () => {
+
   return (
-    <nav className={className}>
-      <a href="/clients">Clients</a>
-      <a href="/vehicules">Vehicules</a>
-      <a href="/locations">Locations</a>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      {/* <!-- Container wrapper --> */}
+      <div className="container-fluid" style={{backgroundColor: 'white'}}>
+        {/* <!-- Toggle button --> */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-mdb-toggle="collapse"
+          data-mdb-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i className="fas fa-bars"></i>
+        </button>
+
+        {/* <!-- Collapsible wrapper --> */}
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          {/* <!-- Navbar brand --> */}
+          <a className="navbar-brand mt-2 mt-lg-0" href="#">
+            <img src={logo} height="120" alt="localib Logo" loading="lazy" />
+          </a>
+          {/* <!-- Left links --> */}
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="/clients">
+                Clients
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/vehicules">
+                Vehicules
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/locations">
+                Locations
+              </a>
+            </li>
+          </ul>
+          {/* <!-- Left links --> */}
+        </div>
+        {/* <!-- Collapsible wrapper --> */}
+      </div>
+      {/* <!-- Container wrapper --> */}
     </nav>
   );
 };
 
-export default NavBar
+export default NavBar;
