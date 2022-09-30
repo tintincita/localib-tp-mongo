2976b0b909448e63d9d732452ca05d5314ca201d
merlink <merlincita@gmail.com>
2022-09-30 15:14:48 +0200

    wip: newLocation queries db. onto create data to test

--- Front/src/pages/newLocation.pages.js
+++ Front/src/pages/newLocation.pages.js
@@ -1,24 +1,37 @@
 import { useState } from "react";
-
+import Button from "react-bootstrap/esm/Button";
 import Card from "react-bootstrap/esm/Card";
 import Form from "react-bootstrap/Form";
 
-const NewLocation = () => {
-    
-    const [location, setLocation] = useState({
-        startDate: "",
-        endDate: "",
-        prixTotal: 0,
-        vehicule: "",
-        client: "",
-      })
+import { getVehiculesAvailable } from "../services/vehicules.services";
 
+const NewLocation = () => {
+  const [location, setLocation] = useState({
+    startDate: "",
+    endDate: "",
+    prixTotal: 0,
+    vehicule: "",
+    client: "",
+  });
+
+  const updateField = (field, value) => {
+    let updatedField = {};
+    updatedField = { [field]: value };
+    setLocation((location) => ({ ...location, ...updatedField }));
+  };
+
+  const handleCheckAvailability = (e) => {
+    e.preventDefault()
+    console.log(e.target);
+    console.log(location);
+    getVehiculesAvailable(location.startDate, location.endDate).then((res)=> console.log("res", res))
+  };
 
   return (
     <Card className="mx-auto" style={{ width: "50%" }}>
       <Card.Body>
-        <Card.Title>Edit Location Details</Card.Title>
-        <Form>
+        <Card.Title>New Location Details</Card.Title>
+        <Form onSubmit={handleCheckAvailability}>
           <Form.Label>Start Date</Form.Label>
           <Form.Control
             type="date"
@@ -33,6 +46,7 @@ const NewLocation = () => {
             value={location.endDate}
             onChange={(e) => updateField("endDate", e.target.value)}
           />
+          <Button type="submit" onSubmit={handleCheckAvailability}>Check availability</Button>
         </Form>
       </Card.Body>
     </Card>
