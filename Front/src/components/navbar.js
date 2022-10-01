0f12a8daa9bab02fce3777f303d9d43b0ab2c5e6
merlincita <merlincita@gmail.com>
2022-10-01 16:25:49 +0200

    Fix: prettier navbar

--- Front/src/components/navbar.js
+++ Front/src/components/navbar.js
@@ -1,55 +1,26 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 import logo from "../assets/localib.png";
 
 const NavBar = () => {
-
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      {/* <!-- Container wrapper --> */}
-      <div className="container-fluid" style={{backgroundColor: 'white'}}>
-        {/* <!-- Toggle button --> */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-mdb-toggle="collapse"
-          data-mdb-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <i className="fas fa-bars"></i>
-        </button>
-
-        {/* <!-- Collapsible wrapper --> */}
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          {/* <!-- Navbar brand --> */}
-          <a className="navbar-brand mt-2 mt-lg-0" href="#">
-            <img src={logo} height="120" alt="localib Logo" loading="lazy" />
-          </a>
-          {/* <!-- Left links --> */}
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" href="/clients">
-                Clients
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/vehicules">
-                Vehicules
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/locations">
-                Locations
-              </a>
-            </li>
-          </ul>
-          {/* <!-- Left links --> */}
-        </div>
-        {/* <!-- Collapsible wrapper --> */}
-      </div>
-      {/* <!-- Container wrapper --> */}
-    </nav>
-  );
+return (
+  <Navbar expand="lg" bg="white">
+      <Container>
+        <Navbar.Brand href="#home"><img src={logo} height="100" alt="localib Logo" loading="lazy" /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/clients">Clients</Nav.Link>
+            <Nav.Link href="/Vehicules">Vehicules</Nav.Link>
+            <Nav.Link href="/Locations">Locations</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+)
 };
 
 export default NavBar;
