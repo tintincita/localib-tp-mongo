14b912d65b7257c3c04496c5ba1e8df1b200d0de
merlincita <merlincita@gmail.com>
2022-10-03 07:03:18 +0200

    Fix: changed to select fields on form_vehicule

--- Front/src/components/vehicules/form_vehicule.js
+++ Front/src/components/vehicules/form_vehicule.js
@@ -2,6 +2,8 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
 import React, { useEffect } from "react";
+import Select from 'react-select';
+
 import { deleteVehicule } from "../../services/vehicules.services";
 
 function VehiculeForm({
@@ -12,7 +14,20 @@ function VehiculeForm({
   setMessage,
   clearForm,
 }) {
-  useEffect(() => {}, [vehicule, setVehicule]);
+  useEffect(() => { }, [vehicule, setVehicule]);
+
+  const optionsEtat = [
+    { value: 'A', label: 'A' },
+    { value: 'B', label: 'B' },
+    { value: 'C', label: 'C' },
+    { value: 'D', label: 'D' }
+  ]
+  const optionsType = [
+    { value: 'Voiture', label: 'Voiture' },
+    { value: 'Camion', label: 'Camion' },
+    { value: 'Utilitaire', label: 'Utilitaire' },
+    { value: 'Moto', label: 'Moto' }
+  ]
 
   const updateField = (field, value) => {
     let updatedField = {};
@@ -33,6 +48,13 @@ function VehiculeForm({
   if (vehicule.id) {
     deleteVehiculeStyle = {};
   }
+
+  const handleEtatSelect = e => {
+    updateField("etat", e.value);
+  };
+  const handleTypeSelect = e => {
+    updateField("type", e.value);
+  };
 
   return (
     <Card className="mx-auto" style={{ width: "50%" }}>
@@ -62,19 +84,14 @@ function VehiculeForm({
             onChange={(e) => updateField("immatriculation", e.target.value)}
           />
           <Form.Label>Etat</Form.Label>
-          <Form.Control
-            type="text"
-            className="mb-3"
-            value={vehicule.etat}
-            onChange={(e) => updateField("etat", e.target.value)}
-          />
+          <Select options={optionsEtat} value={optionsEtat.filter(function(option) {
+          return option.value === vehicule.etat;
+        })} onChange= {handleEtatSelect}/>
+
           <Form.Label>Type</Form.Label>
-          <Form.Control
-            type="text"
-            className="mb-3"
-            value={vehicule.type}
-            onChange={(e) => updateField("type", e.target.value)}
-          />
+          <Select options={optionsType} value={optionsType.filter(function(option) {
+          return option.value === vehicule.type;
+        })} onChange= {handleTypeSelect}/>
           <Form.Label>Prix Journée</Form.Label>
           <Form.Control
             type="number"
