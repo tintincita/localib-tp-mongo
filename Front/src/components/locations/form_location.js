fd600426cc7e81c929c9781adaa4951e13a528df
merlincita <merlincita@gmail.com>
2022-10-12 11:19:54 +0200

    Fix: Bug on Location Form

--- Front/src/components/locations/form_location.js
+++ Front/src/components/locations/form_location.js
@@ -1,37 +1,42 @@
 import Card from "react-bootstrap/esm/Card";
-import Form from "react-bootstrap/Form";
+import CardGroup from 'react-bootstrap/CardGroup';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 
 function LocationForm({ location, setLocation }) {
-    
-    const updateField = (field, value) => {
-      let updatedField = {};
-      updatedField = { [field]: value };
-      setLocation((location) => ({ ...location, ...updatedField }));
-    };
+
+  const updateField = (field, value) => {
+    let updatedField = {};
+    updatedField = { [field]: value };
+    setLocation((location) => ({ ...location, ...updatedField }));
+  };
 
   return (
-    <Card className="mx-auto" style={{ width: "50%" }}>
-      <Card.Body>
-        <Card.Title>Location Details</Card.Title>
-        <Form>
-          <Form.Label>Start Date</Form.Label>
-          <Form.Control
-            type="date"
-            className="mb-3"
-            value={location.startDate}
-            onChange={(e) => updateField("startDate", e.target.value)}
-          />
-          <Form.Label>End Date</Form.Label>
-          <Form.Control
-            type="date"
-            className="mb-3"
-            value={location.endDate}
-            onChange={(e) => updateField("endDate", e.target.value)}
-          />
-        </Form>
-      </Card.Body>
-    </Card>
+    <><h1>Location Details</h1>
+      <CardGroup >
+        <Card className="mx-auto">
+          <Card.Body>
+            <Card.Header>CLIENT</Card.Header>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>Full Name: {location.client.fullName}</ListGroup.Item>
+              <ListGroup.Item>Date of Birth: {location.client.dob}</ListGroup.Item>
+              <ListGroup.Item>eMail: {location.client.email}</ListGroup.Item>
+              <ListGroup.Item>phone: {location.client.phone}</ListGroup.Item>
+            </ListGroup>
+          </Card.Body>
+        </Card>
+        <Card className="mx-auto">
+          <Card.Header>VEHICULE</Card.Header>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>Marque: {location.vehicule.marque}</ListGroup.Item>
+            <ListGroup.Item>Model: {location.vehicule.model}</ListGroup.Item>
+            <ListGroup.Item>Immatriculation: {location.vehicule.immatriculation}</ListGroup.Item>
+            <ListGroup.Item>Etat: {location.vehicule.etat}</ListGroup.Item>
+            <ListGroup.Item>Type: {location.vehicule.type}</ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </CardGroup>
+    </>
   );
 }
 
