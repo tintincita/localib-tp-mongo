8e92bed845a6eabc98ebd08fba62777ddaff7d9e
merlincita <merlincita@gmail.com>
2022-10-12 11:46:58 +0200

    Fix: bug when no clients in db

--- Front/src/components/clients/listing_clients.js
+++ Front/src/components/clients/listing_clients.js
@@ -6,26 +6,26 @@ import { getClients } from "../../services/client.services";
 
 const ListingClients = ({ records, setRecords, handleClientClick }) => {
 
-  useEffect( () => {
+  useEffect(() => {
     getClients().then(records => setRecords(records))
-
   }, []);
 
   const renderListing = () => {
     let recordList = [];
-
-    records.map((record) => {
-      return recordList.push(
-        <tr key={record.id}  onClick={() => handleClientClick(record)}>
-          <td>{record.fullName}</td>
-          <td>
-            <Moment format="DD-MM-YYYY">{record.dob}</Moment>
-          </td>
-          <td>{record.email}</td>
-          <td>{record.phone}</td>
-        </tr>
-      );
-    });
+    if (records) {
+      records.map((record) => {
+        return recordList.push(
+          <tr key={record.id} onClick={() => handleClientClick(record)}>
+            <td>{record.fullName}</td>
+            <td>
+              <Moment format="DD-MM-YYYY">{record.dob}</Moment>
+            </td>
+            <td>{record.email}</td>
+            <td>{record.phone}</td>
+          </tr>
+        );
+      });
+    }
     return recordList;
   };
 
